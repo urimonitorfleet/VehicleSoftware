2d52c9332b33ef7b09d7c4e42865828412ba6111
Brian Kintz <bkintz@ele.uri.edu>
2012-03-14 22:33:18 -0400

    Initial Commit
    GPS Plugin:
    
    Parses streaming serial data from /dev/GPS and write it to a plugin-formatted file in the location specified in config.pl
    
    Currently supports:
    
    Latitude
    Longitude
    Heading (True)
    Heading (Magnetic)
    Speed Over Ground (Kts)
    Fix Quality (None|Normal|Differential)
    Satellites In Use

---
+++ control_auto/plugins/gps/gather.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+# Brian Kintz
+# 12.03.2012
+
+# system/gather.pl
+
+# data collection plugin - system information
+
+# collect system information and make it accessible to the main control program
+
+# currently collected:  cpu usage, memory usage, cpu temperature
+
+use strict;
+use Switch;
+use Fcntl qw(:flock SEEK_END);
+
+require '/root/code/control_auto/config.pl';
+
+my ($fixQual, $lat, $NS, $long, $EW, $satInUse);
+my ($hdg_mag, $hdg_true, $SOG_kts);
+my (@data, $count);
+
+our $GPS_DATA;
+
+my @fixQualities = ( 'None', 'Standard GPS Fix', 'Differential GPS Fix' );
+
+open(GPS, "</dev/GPS");
+
+$count = 0;
+
+while(1){
+   while(<GPS>){
+      @data = split(',', $_);
+      
+      chomp @data;
+
+      switch ($data[0]) {
+         case '$GPGGA' {
+            $fixQual = $fixQualities[$data[6]];
+            $lat = $data[2] || '-1';
+            $NS = $data[3] || '-';
+            $long = $data[4] || '-1';
+            $EW = $data[5] || '-';
+            $satInUse = $data[7] || '0';
+         }
+
+         case '$HCHDG' { $hdg_mag  = $data[1] || '-1'; }
+         case '$HCHDT' { $hdg_true = $data[1] || '-1'; }
+         case '$GPVTG' { $SOG_kts  = $data[5] || '-1'; }
+      }
+
+      if(++$count == 4){
+         writeData();
+         $count = 0;
+      }
+   }
+}
+
+sub writeData {
+   # open+lock file for writing
+   open(FH, ">$GPS_DATA") or die "cannot open > $GPS_DATA";
+   flock(FH, LOCK_EX) or die "Cannot lock file $GPS_DATA - $!\n";
+   
+   print FH "gps_quality|$fixQual\n";
+   print FH "gps_lat|$lat\n";
+   print FH "gps_long|$long\n";
+   print FH "gps_satInUse|$satInUse\n";
+   print FH "gps_hdg_true|$hdg_true\n";
+   print FH "gps_hdg_mag|$hdg_mag\n";
+   print FH "gps_sog_kts|$SOG_kts\n";
+
+   close FH;
+}
+
